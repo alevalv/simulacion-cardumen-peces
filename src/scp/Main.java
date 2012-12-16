9d063d8be3527e1575af19f2db3542134ba1d422
Cristina Bustos <mcb9216@hotmail.com>
2012-12-16 03:34:04 -0500

    main con la mierda grafica

--- src/scp/Main.java
+++ src/scp/Main.java
@@ -1,8 +1,32 @@
 
 package scp;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import scp.interfaz.Estilo;
+import scp.interfaz.InterfazTotal;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("hi");
+
+        public static void main(String a[]){
+         MetalLookAndFeel.setCurrentTheme(new Estilo());
+      try{
+         UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+      }catch (Exception ex){
+         System.out.println("Falló la carga del tema");
+         System.out.println(ex);
+      }
+      JFrame.setDefaultLookAndFeelDecorated(true);
+      JDialog.setDefaultLookAndFeelDecorated(true);
+      try{
+         UIManager.setLookAndFeel(new MetalLookAndFeel());
+      }catch ( UnsupportedLookAndFeelException e ){
+         System.out.println ("Metal Look & Feel no es soportada en esta plataforma");
+         System.exit(0);
+      }
+        InterfazTotal Inter=new InterfazTotal();
     }
+
 }

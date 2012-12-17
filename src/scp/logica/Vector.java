561d56229c5812e15f37d7baffa1d730868d7891
Alejandro Valdés <alejandrovaldes@live.com>
2012-12-16 21:30:31 -0500

    agregada clase Vector.java, integrada en Pez.java

---
+++ src/scp/logica/Vector.java
@@ -0,0 +1,31 @@
+package scp.logica;
+
+public class Vector {
+    private double x;
+    private double y;
+
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    
+    public double magnitud(){
+        return Math.sqrt(x*x + y*y);
+    }
+    
+    public static Vector add(Vector v1, Vector v2){
+        return new Vector((v1.getX()+v2.getX()), (v1.getY()+v2.getY()));
+    }
+    
+    public static Vector div(Vector v1, double A){
+        return new Vector(v1.getX()/A, v1.getY()/A);
+    }
+}

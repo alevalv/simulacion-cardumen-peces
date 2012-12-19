ee34712e3e4c00ff17759769d8fbbae5146e2889
Cristina Bustos <mcb9216@hotmail.com>
2012-12-19 01:35:30 -0500

    peces en imagenes

--- src/scp/interfaz/micanvas.java
+++ src/scp/interfaz/micanvas.java
@@ -6,16 +6,14 @@ package scp.interfaz;
 
 
 
-import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.util.Vector;
+import javax.swing.ImageIcon;
 import javax.swing.Timer;
 
 
@@ -38,17 +36,20 @@ public class micanvas extends Canvas implements ActionListener, MouseListener{
     //se crea un timer que manejara todas la bolitas
     private Dimension d;
     Timer time;
+    ImageIcon imagenpez;
+    
     micanvas(int x, int y){
         setBackground(Color.white);
         setSize(x,y);
         time = new Timer(5, this);
         d=this.getSize();
+        imagenpez=  new ImageIcon(getClass().getResource("fish.png"));
         Pezh.limx=d.width;
         Pezh.limy=d.height;
         this.addMouseListener(this);
         imagen = new BufferedImage(d.width,d.height, BufferedImage.TYPE_INT_RGB);
         bufer=imagen.getGraphics();
-        for(int a=0; a<1;a++){
+        for(int a=0; a<10;a++){
             peces.add(new Pezh(Pezh.gen.nextInt(d.width),Pezh.gen.nextInt(d.height)));
         }
         
@@ -59,15 +60,17 @@ public class micanvas extends Canvas implements ActionListener, MouseListener{
         int tailx=0, taily=0;
         //Usando un bufferedimage evitamos el parpadeo de la pantalla
         bufer.clearRect(0, 0, d.width, d.height);
-        bufer.setColor(Color.black);
+        bufer.setColor(Color.white);
         bufer.fillRect(0, 0, d.width, d.height);
         //se dibujan las bolitas en imagen
         for(int i=0; i<peces.size(); i++){
-            bufer.setColor(peces.elementAt(i).Color());
-            bufer.fillOval(peces.elementAt(i).x(), peces.elementAt(i).y(), 14, 14);
-            tailx=TailX(peces.elementAt(i).getDireccion());
-            taily=TailY(peces.elementAt(i).getDireccion());
-            bufer.fillOval(peces.elementAt(i).x()+tailx, peces.elementAt(i).y()+taily, 7, 7);
+            bufer.drawImage(imagenpez.getImage(), peces.elementAt(i).x(), peces.elementAt(i).y(), this);
+            //bufer.setColor(peces.elementAt(i).Color());
+            //bufer.fillOval(peces.elementAt(i).x(), peces.elementAt(i).y(), 14, 14);
+            //tailx=TailX(peces.elementAt(i).getDireccion());
+            //taily=TailY(peces.elementAt(i).getDireccion());
+            //bufer.fillOval(peces.elementAt(i).x()+tailx, peces.elementAt(i).y()+taily, 7, 7);
+           
         }
         //dibujamos la buffered image en pantalla
         g.drawImage(imagen, 0, 0, this);

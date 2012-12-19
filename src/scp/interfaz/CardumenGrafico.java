4972afcb1555ae4c125a819b9f85dadfa35f3471
Alejandro Valdés <alejandrovaldes@live.com>
2012-12-19 13:55:29 -0500

    agregado clase para pintar el cardumen

---
+++ src/scp/interfaz/CardumenGrafico.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scp.interfaz;
+
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import scp.logica.Cardumen;
+
+/**
+ *
+ * @author alevalv
+ */
+public class CardumenGrafico {
+    private ArrayList<BufferedImage> imagenesPeces;
+    void CardumenGrafico() throws IOException {
+        imagenesPeces = new ArrayList<>(360);
+        imagenesPeces.add(ImageIO.read(getClass().getResource("fish.png")));
+    }
+    
+    private void inicializarPeces(){
+        for(int i=1;i<360;i++){
+            AffineTransform tx = new AffineTransform();
+            // last, width = height and height = width :)
+            tx.translate(imagenesPeces.get(0).getHeight() / 2,imagenesPeces.get(0).getWidth() / 2);
+            tx.rotate(Math.PI / 2);
+            // first - center image at the origin so rotate works OK
+            tx.translate(-imagenesPeces.get(0).getWidth() / 2,-imagenesPeces.get(0).getHeight() / 2);
+            AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+            // new destination image where height = width and width = height.
+            BufferedImage nuevoPez =new BufferedImage(imagenesPeces.get(0).getHeight(), imagenesPeces.get(0).getWidth(), imagenesPeces.get(0).getType());
+            op.filter(imagenesPeces.get(0), nuevoPez);
+            imagenesPeces.add(nuevoPez);
+        }
+    }
+    
+    public void pintarCardumen(Cardumen cardumen){
+        
+    }
+}
